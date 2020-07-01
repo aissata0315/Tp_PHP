65bec19abd1911bfee197d4a7726686aabe9e643
Aissata Ba <aissata.ba0903@gmail.com>
2020-07-01 14:45:03 +0000

    dossier procedural

--- Procedural/creerCompte.php
+++ Procedural/creerCompte.php
@@ -19,20 +19,13 @@
                 '{$_POST['id_client']}',
                 '{$_POST['dateOuverture']}',
                 '{$_POST['dateFin']}'
-            
-
             )";
-            
-    
         //excecusion de la requette dans la base
         mysqli_query($idCon, $requete) or die("erreur requete: ".mysqli_error($idCon));
         $msg = "<h3 style='color: '>Enregistrement reussi</h3>";
         echo $msg;
         header('location: compte.php?succes=ok');
-
-    
     }
-    
     if(!empty($_POST['form3'])){
         //var_dump($_POST);  
         //requete creer compte
@@ -46,8 +39,6 @@
                 null,
                 '{$_POST['dateOuverture']}',
                 '{$_POST['dateFin']}'
-            
-
             )";
             var_dump($_POST);
     
@@ -55,6 +46,7 @@
         mysqli_query($idCon, $requete2) or die("erreur requete: ".mysqli_error($idCon));
         $msg = "<h3 style='color: '>Enregistrement reussi</h3>";
         echo $msg;
+        header('location: compte.php?succes=ok');
 
     
     }
--- Procedural/traitement.php
+++ Procedural/traitement.php
@@ -39,8 +39,7 @@ if(!empty($_POST['form2'])){
         NULL,
         '{$_POST['nom']}',
         '{$_POST['prenom']}',
-        '{$_POST['adresse']}'
-        '',
+        '{$_POST['adresse']}',
         '{$_POST['email']}',
         '{$_POST['telephone']}',
         '{$_POST['salaire']}',
@@ -55,6 +54,7 @@ if(!empty($_POST['form2'])){
     mysqli_query($idCon, $requete3) or die("erreur requete: ".mysqli_error($idCon));
     $msg = "<h3 style='color: '>Enregistrement reussi</h3>";
     echo $msg;
+    header('location: client.php?succes=ok');
 
 }
 ?>
